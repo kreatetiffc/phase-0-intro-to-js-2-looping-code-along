30f12788f1232b531d27c533174c1038c3b1845a
Tiffany Crooks <tiffcrooks2011@gmail.com>
2023-03-22 18:53:57 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const birthday = ["Charli", "Samip", "Ali"]
+
+function writeCards(birthday) {
+    for (let n = 0; n < birthday.length; n++) {
+        console.log(`Thank you, ${birthday[n]}, for the wonderful birthday gift!`)
+        debugger
+    }
+
+    return birthday
+}
+
+writeCards(birthday)
+
+let countDown = 10
+while (countDown > -1) {
+    console.log(countDown--)
+}
